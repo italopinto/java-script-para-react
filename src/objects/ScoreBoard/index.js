f473c783a874d015a1a9fe56f49b42af68818295
italo <italosp00@gmail.com>
2021-07-11 13:00:42 -0300

    :sparkles: Add ScoreBoard CSS.

--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,10 +1,11 @@
 import PlayerName from "../../components/PlayerName";
+import "./style.css"
 
 function ScoreBoard() {
   return /*html*/`
   <header class="score-board">
-    ${PlayerName("Player 1")}
-    ${PlayerName("Player 2")}
+    ${PlayerName("Player1")}
+    ${PlayerName("Player2")}
   </header>
   `;
 }
